eeb1cf0456bcba9d18162b3bc7e6a7f25a54ba8f
et357 <peter001019@naver.com>
2024-04-14 16:44:08 +0900

    [setting] 초기 세팅

---
+++ server/src/main/java/com/example/menuw/MenuwApplication.java
@@ -0,0 +1,13 @@
+package com.example.menuw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuwApplication.class, args);
+    }
+
+}
