f383535cf2da35f9446fd9e0d2361c0fe8004027
emjayMJkim <minjeong030420@gmail.com>
2024-04-17 05:06:18 +0900

    #4 [feat] bottom navi 전환 구현

--- client/app/src/main/java/com/kaer/menuw/presentation/person/PersonFragment.kt
+++ client/app/src/main/java/com/kaer/menuw/presentation/person/PersonFragment.kt
@@ -1,4 +1,12 @@
 package com.kaer.menuw.presentation.person
 
-class PersonFragment {
+import com.kaer.menuw.R
+import com.kaer.menuw.databinding.FragmentPersonBinding
+import com.kaer.menuw.util.base.BaseFragment
+
+class PersonFragment: BaseFragment<FragmentPersonBinding>(R.layout.fragment_person) {
+
+    override fun onViewCreated(view: android.view.View, savedInstanceState: android.os.Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/RefrigeratorFragment.kt
+++ client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/RefrigeratorFragment.kt
@@ -1,4 +1,14 @@
 package com.kaer.menuw.presentation.refrigerator
 
-class RefrigeratorFragment {
+import android.os.Bundle
+import android.view.View
+import com.kaer.menuw.R
+import com.kaer.menuw.databinding.FragmentRefrigeratorBinding
+import com.kaer.menuw.util.base.BaseFragment
+
+class RefrigeratorFragment: BaseFragment<FragmentRefrigeratorBinding>(R.layout.fragment_refrigerator) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
