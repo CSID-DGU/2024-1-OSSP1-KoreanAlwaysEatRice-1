ea5bc05919fb99c04d9854393caf35d29f316022
emjayMJkim <minjeong030420@gmail.com>
2024-04-17 15:07:23 +0900

    #4 [feat] 냉장고 페이지 연결

--- client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/RefrigeratorFragment.kt
+++ client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/RefrigeratorFragment.kt
@@ -1,14 +1,29 @@
 package com.kaer.menuw.presentation.refrigerator
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.kaer.menuw.R
 import com.kaer.menuw.databinding.FragmentRefrigeratorBinding
+import com.kaer.menuw.presentation.refrigerator.add.AddIngredientActivity
 import com.kaer.menuw.util.base.BaseFragment
 
 class RefrigeratorFragment: BaseFragment<FragmentRefrigeratorBinding>(R.layout.fragment_refrigerator) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        clickAddIngredientBtn()
+    }
+
+    private fun clickSeeRecommendBtn() {
+        //
+    }
+
+    private fun clickAddIngredientBtn() {
+        val intent = Intent(requireActivity(), AddIngredientActivity::class.java)
+        binding.btnRefrigeratorAddIngredient.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
---
+++ client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/add/AddIngredientActivity.kt
@@ -0,0 +1,14 @@
+package com.kaer.menuw.presentation.refrigerator.add
+
+import android.os.Bundle
+import com.kaer.menuw.R
+import com.kaer.menuw.databinding.ActivityAddIngredientBinding
+import com.kaer.menuw.util.base.BaseActivity
+
+class AddIngredientActivity :
+    BaseActivity<ActivityAddIngredientBinding>(R.layout.activity_add_ingredient) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
