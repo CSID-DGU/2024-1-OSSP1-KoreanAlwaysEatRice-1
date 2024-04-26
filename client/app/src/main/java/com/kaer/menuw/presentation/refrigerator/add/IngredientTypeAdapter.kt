2300dd25312d60d98412115535e205dfa6de00fe
emjayMJkim <minjeong030420@gmail.com>
2024-04-27 01:04:57 +0900

    #4 [feat] 재료 타입 색상 변경

--- client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/add/IngredientTypeAdapter.kt
+++ client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/add/IngredientTypeAdapter.kt
@@ -2,8 +2,10 @@ package com.kaer.menuw.presentation.refrigerator.add
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.kaer.menuw.R
 import com.kaer.menuw.databinding.ItemIngredientTypeBinding
 import com.kaer.menuw.domain.entity.IngredientTotal
 import com.kaer.menuw.util.ItemDiffCallback
@@ -31,16 +33,47 @@ class IngredientTypeAdapter :
         private val binding: ItemIngredientTypeBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: IngredientTotal) {
-            binding.tvItemIngredientTypeName.text = data.type
+            binding.item = data
+
+            if (selectedPosition == absoluteAdapterPosition) {
+                changeBackground(binding, true)
+                changeTextColor(binding, true)
+            } else {
+                changeBackground(binding, false)
+                changeTextColor(binding, false)
+            }
 
             if (onItemClickListener != null) {
                 binding.root.setOnClickListener {
                     onItemClickListener?.onItemClick(data, absoluteAdapterPosition)
                     if (selectedPosition != absoluteAdapterPosition) {
+                        changeBackground(binding, true)
+                        changeTextColor(binding, true)
+                        notifyItemChanged(selectedPosition)
                         selectedPosition = absoluteAdapterPosition
                     }
                 }
             }
+        }
+    }
+
+    private fun changeBackground(
+        binding: ItemIngredientTypeBinding,
+        selected: Boolean
+    ) {
+        when (selected) {
+            true -> binding.tvItemIngredientTypeName.setBackgroundResource(R.drawable.shape_green_fill_10_rect)
+            false -> binding.tvItemIngredientTypeName.setBackgroundResource(R.drawable.shape_gray_fill_10_rect)
+        }
+    }
+
+    private fun changeTextColor(
+        binding: ItemIngredientTypeBinding,
+        selected: Boolean
+    ) {
+        when (selected) {
+            true -> binding.tvItemIngredientTypeName.setTextColor(ContextCompat.getColor(binding.root.context, R.color.bright))
+            false -> binding.tvItemIngredientTypeName.setTextColor(ContextCompat.getColor(binding.root.context, R.color.green_primary))
         }
     }
 
