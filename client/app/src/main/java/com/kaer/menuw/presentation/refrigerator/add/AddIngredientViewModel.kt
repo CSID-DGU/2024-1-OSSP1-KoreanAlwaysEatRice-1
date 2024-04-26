ac712b00fe7f9653ed121707d3abac5db2a13bf7
emjayMJkim <minjeong030420@gmail.com>
2024-04-26 23:19:31 +0900

    #4 [mod] 더미데이터 & UI 수정

--- client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/add/AddIngredientViewModel.kt
+++ client/app/src/main/java/com/kaer/menuw/presentation/refrigerator/add/AddIngredientViewModel.kt
@@ -13,57 +13,54 @@ class AddIngredientViewModel: ViewModel() {
                 "채소",
                 arrayListOf(
                     IngredientTotal.IngredientItem(1, "가지", ""),
-                    IngredientTotal.IngredientItem(2, "양상추", "")
+                    IngredientTotal.IngredientItem(2, "양상추", ""),
+                    IngredientTotal.IngredientItem(3, "양상추", ""),
+                    IngredientTotal.IngredientItem(4, "양상추", ""),
+                    IngredientTotal.IngredientItem(5, "양상추", ""),
+                    IngredientTotal.IngredientItem(6, "양상추", ""),
+                    IngredientTotal.IngredientItem(7, "양상추", ""),
+                    IngredientTotal.IngredientItem(8, "양상추", ""),
+                    IngredientTotal.IngredientItem(9, "양상추", ""),
+                    IngredientTotal.IngredientItem(10, "양상추", ""),
+                    IngredientTotal.IngredientItem(11, "양상추", ""),
+                    IngredientTotal.IngredientItem(12, "양상추", ""),
+                    IngredientTotal.IngredientItem(13, "양상추", ""),
                 )
             ),
             IngredientTotal(
                 "유제품",
                 arrayListOf(
-                    IngredientTotal.IngredientItem(3, "우유", ""),
-                    IngredientTotal.IngredientItem(4, "버터", "")
+                    IngredientTotal.IngredientItem(14, "우유", ""),
+                    IngredientTotal.IngredientItem(15, "버터", "")
                 )
             ),
             IngredientTotal(
-                "버섯류",
+                "곡류",
                 arrayListOf(
-                    IngredientTotal.IngredientItem(5, "새송이", ""),
-                    IngredientTotal.IngredientItem(6, "표고", ""),
-                    IngredientTotal.IngredientItem(7, "느타리", "")
+                    IngredientTotal.IngredientItem(16, "쌀", ""),
+                    IngredientTotal.IngredientItem(17, "보리", ""),
+                    IngredientTotal.IngredientItem(18, "귀리", "")
                 )
             ),
             IngredientTotal(
-                "유지류",
+                "육류",
                 arrayListOf(
-                    IngredientTotal.IngredientItem(8, "카놀라유", ""),
-                    IngredientTotal.IngredientItem(9, "식용유", "")
+                    IngredientTotal.IngredientItem(19, "카놀라유", ""),
+                    IngredientTotal.IngredientItem(20, "식용유", "")
                 )
             ),
             IngredientTotal(
-                "시험",
+                "생선",
                 arrayListOf(
-                    IngredientTotal.IngredientItem(10, "끝났다", ""),
-                    IngredientTotal.IngredientItem(11, "행복해", "")
+                    IngredientTotal.IngredientItem(21, "고등어", ""),
+                    IngredientTotal.IngredientItem(22, "연어", "")
                 )
             ),
             IngredientTotal(
-                "이제",
+                "기타",
                 arrayListOf(
-                    IngredientTotal.IngredientItem(12, "개발", ""),
-                    IngredientTotal.IngredientItem(13, "시작", "")
-                )
-            ),
-            IngredientTotal(
-                "더미",
-                arrayListOf(
-                    IngredientTotal.IngredientItem(14, "데이터", ""),
-                    IngredientTotal.IngredientItem(15, "뭐넣지", "")
-                )
-            ),
-            IngredientTotal(
-                "생각이",
-                arrayListOf(
-                    IngredientTotal.IngredientItem(16, "더이상", ""),
-                    IngredientTotal.IngredientItem(17, "안나요", "")
+                    IngredientTotal.IngredientItem(23, "기타", ""),
+                    IngredientTotal.IngredientItem(24, "기타", "")
                 )
             ),
         )
