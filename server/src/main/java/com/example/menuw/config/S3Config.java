4beacbcc9063e7a7269198b77bf4476aab6f446a
et357 <peter001019@naver.com>
2024-05-01 21:45:48 +0900

    #8 [feat] adding S3UploadService

--- server/src/main/java/com/example/menuw/config/S3Config.java
+++ server/src/main/java/com/example/menuw/config/S3Config.java
@@ -4,7 +4,7 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
