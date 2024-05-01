dd76e3f30b1260629b7ff858c3449eb4785a43b3
et357 <peter001019@naver.com>
2024-05-01 17:19:06 +0900

    #8 [feat] adding RefrigeratorController

---
+++ server/src/main/java/com/example/menuw/web/RefrigeratorController.java
@@ -0,0 +1,20 @@
+package com.example.menuw.web;
+
+import com.example.menuw.domain.Ingredient;
+import com.example.menuw.service.IngredientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class RefrigeratorController {
+    private final IngredientService ingredientService;
+
+    @GetMapping("/refrigerator")
+    public List<Ingredient> findAllIngredients(Ingredient ingredient) {
+        return ingredientService.findIngredients();
+    }
+}
