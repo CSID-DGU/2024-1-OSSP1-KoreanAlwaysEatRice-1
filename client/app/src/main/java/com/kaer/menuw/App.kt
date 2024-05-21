eeffcd34716698463488f022610b6cf596d49f3d
emjayMJkim <minjeong030420@gmail.com>
2024-05-21 16:19:57 +0900

    #22 [feat] kakao init

--- client/app/src/main/java/com/kaer/menuw/App.kt
+++ client/app/src/main/java/com/kaer/menuw/App.kt
@@ -2,6 +2,8 @@ package com.kaer.menuw
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.kaer.menuw.BuildConfig.NATIVE_APP_KEY
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -10,6 +12,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this, NATIVE_APP_KEY)
 
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
