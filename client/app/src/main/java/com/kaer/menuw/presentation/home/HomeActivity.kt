32c05b332ede63e77e9c90553b35c4d4eed8882e
emjayMJkim <minjeong030420@gmail.com>
2024-05-23 03:30:15 +0900

    #22 [feat] homeActivity 생성

---
+++ client/app/src/main/java/com/kaer/menuw/presentation/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.kaer.menuw.presentation.home
+
+import android.os.Bundle
+import com.kaer.menuw.R
+import com.kaer.menuw.databinding.ActivityHomeBinding
+import com.kaer.menuw.util.base.BaseActivity
+
+class HomeActivity: BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
