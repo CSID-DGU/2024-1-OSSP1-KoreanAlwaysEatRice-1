0adffb6aed97a20e07e90b6f484adc6158777720
emjayMJkim <minjeong030420@gmail.com>
2024-05-23 03:30:05 +0900

    #22 [feat] kakaoLogIn token&callback 과정

--- client/app/src/main/java/com/kaer/menuw/presentation/onboarding/login/LogInActivity.kt
+++ client/app/src/main/java/com/kaer/menuw/presentation/onboarding/login/LogInActivity.kt
@@ -1,24 +1,54 @@
 package com.kaer.menuw.presentation.onboarding.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.kaer.menuw.R
+import com.kaer.menuw.data.service.KakaoAuthService
 import com.kaer.menuw.databinding.ActivityLoginBinding
+import com.kaer.menuw.presentation.home.HomeActivity
 import com.kaer.menuw.util.base.BaseActivity
+import com.kakao.sdk.user.UserApiClient
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LogInActivity: BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    @Inject
+    lateinit var kakaoAuthService: KakaoAuthService
 
     private val viewModel by viewModels<LogInViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startKakaoLogIn()
+        UserApiClient.instance.accessTokenInfo {tokenInfo, error ->
+            if (error != null) {
+                startKakaoLogIn()
+                isKakaoLogInSuccess()
+            } else if (tokenInfo != null) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+//        startKakaoLogIn()
     }
 
     private fun  startKakaoLogIn() {
         binding.btnKakaoLogIn.setOnClickListener {
-            //
+            kakaoAuthService.startKakaoLogIn(viewModel.kakaoLogInCallback)
+        }
+    }
+
+    private fun isKakaoLogInSuccess() {
+        viewModel.isKakaoLogInSuccess.observe(this) {
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- client/app/src/main/java/com/kaer/menuw/presentation/onboarding/login/LogInViewModel.kt
+++ client/app/src/main/java/com/kaer/menuw/presentation/onboarding/login/LogInViewModel.kt
@@ -1,6 +1,28 @@
 package com.kaer.menuw.presentation.onboarding.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kaer.menuw.util.KakaoLogInCallback
+import com.kakao.sdk.auth.model.OAuthToken
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 
-class LogInViewModel: ViewModel() {
+@HiltViewModel
+class LogInViewModel : ViewModel() {
+
+    private val _isKakaoLogInSuccess = MutableLiveData<Boolean>()
+    val isKakaoLogInSuccess: LiveData<Boolean>
+        get() = _isKakaoLogInSuccess
+
+    val kakaoLogInCallback: (OAuthToken?, Throwable?) -> Unit = { oAuthToken, error ->
+        KakaoLogInCallback {
+            viewModelScope.launch {
+                oAuthToken.let {
+//                    _isKakaoLogInSuccess.value =
+                }
+            }
+        }.setKakaoCallback(oAuthToken, error)
+    }
 }
