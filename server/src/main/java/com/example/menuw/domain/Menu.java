50784dc5b9d0d611924890a0f5fabf8d4135ff0f
et357 <peter001019@naver.com>
2024-05-22 14:31:12 +0900

    #24 [mod] modify response

---
+++ server/src/main/java/com/example/menuw/domain/Menu.java
@@ -0,0 +1,20 @@
+package com.example.menuw.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Menu {
+    @Id @GeneratedValue
+    private Integer menuId;
+    private Integer menuLike;
+    private String menuName;
+    private String menuImageURL;
+    private String ingredients;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+}
