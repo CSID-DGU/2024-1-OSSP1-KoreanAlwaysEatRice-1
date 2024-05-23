e21943ae07170b08739a2d85869673c88dafa88f
sunhanmoo <ghdms1013@naver.com>
2024-05-23 22:30:35 +0900

    #28 [add] 액티비티 파일 추가

---
+++ client/app/src/main/java/com/kaer/menuw/presentation/mypage/MyPage.kt
@@ -0,0 +1,16 @@
+package com.kaer.menuw.presentation.mypage
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kaer.menuw.R
+import com.kaer.menuw.databinding.ActivityMainBinding
+import com.kaer.menuw.util.base.BaseActivity
+
+class MyPage : BaseActivity<ActivityMainBinding>(R.layout.activity_mypage_layout) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
