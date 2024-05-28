3a58d9c5de5a0f869321c7c41985e5bd0daab83d
sunhanmoo <ghdms1013@naver.com>
2024-05-28 13:01:05 +0900

    #41 [feat] 스플래시뷰 액티비티 추가

---
+++ client/app/src/main/java/com/kaer/menuw/presentation/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.kaer.menuw.presentation.splash
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kaer.menuw.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
