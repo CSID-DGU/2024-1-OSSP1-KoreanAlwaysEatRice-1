2b111702bc61d23d3d5a31b8aaed4c2a623bac14
emjayMJkim <minjeong030420@gmail.com>
2024-05-28 18:15:54 +0900

    #42 [mod] request body 변경

--- client/app/src/main/java/com/kaer/menuw/data/datasource/impl/MenuDataSourceImpl.kt
+++ client/app/src/main/java/com/kaer/menuw/data/datasource/impl/MenuDataSourceImpl.kt
@@ -1,11 +1,15 @@
 package com.kaer.menuw.data.datasource.impl
 
 import com.kaer.menuw.data.datasource.MenuDataSource
+import com.kaer.menuw.data.model.remote.request.RequestPostRecipeDto
 import com.kaer.menuw.data.model.remote.response.ResponsePostRecipeDto
 import com.kaer.menuw.data.service.MenuService
 import javax.inject.Inject
 
-class MenuDataSourceImpl @Inject constructor(private val apiService: MenuService): MenuDataSource {
+class MenuDataSourceImpl @Inject constructor(private val apiService: MenuService) : MenuDataSource {
 
-    override suspend fun postMenuRecipe(menuName: String): ResponsePostRecipeDto = apiService.postMenuRecipe(menuName)
+    override suspend fun postMenuRecipe(menuName: String): ResponsePostRecipeDto =
+        apiService.postMenuRecipe(
+            RequestPostRecipeDto(menuName)
+        )
 }
--- client/app/src/main/java/com/kaer/menuw/data/service/MenuService.kt
+++ client/app/src/main/java/com/kaer/menuw/data/service/MenuService.kt
@@ -1,5 +1,6 @@
 package com.kaer.menuw.data.service
 
+import com.kaer.menuw.data.model.remote.request.RequestPostRecipeDto
 import com.kaer.menuw.data.model.remote.response.ResponsePostRecipeDto
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -7,5 +8,5 @@ import retrofit2.http.POST
 interface MenuService {
 
     @POST("/menu/recipe")
-    suspend fun postMenuRecipe(@Body menuName: String): ResponsePostRecipeDto
+    suspend fun postMenuRecipe(@Body request: RequestPostRecipeDto): ResponsePostRecipeDto
 }
