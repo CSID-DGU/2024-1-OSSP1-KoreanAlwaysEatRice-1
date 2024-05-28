dc01026efdbf00651036f218ea617e983c17617e
emjayMJkim <minjeong030420@gmail.com>
2024-05-28 17:43:50 +0900

    #42 [add] dataSource & repository 생성

---
+++ client/app/src/main/java/com/kaer/menuw/data/datasource/impl/MenuDataSourceImpl.kt
@@ -0,0 +1,11 @@
+package com.kaer.menuw.data.datasource.impl
+
+import com.kaer.menuw.data.datasource.MenuDataSource
+import com.kaer.menuw.data.model.remote.response.ResponsePostRecipeDto
+import com.kaer.menuw.data.service.MenuService
+import javax.inject.Inject
+
+class MenuDataSourceImpl @Inject constructor(private val apiService: MenuService): MenuDataSource {
+
+    override suspend fun postMenuRecipe(menuName: String): ResponsePostRecipeDto = apiService.postMenuRecipe(menuName)
+}
---
+++ client/app/src/main/java/com/kaer/menuw/data/repository/MenuRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package com.kaer.menuw.data.repository
+
+import com.kaer.menuw.data.datasource.MenuDataSource
+import com.kaer.menuw.domain.entity.RecipeList
+import com.kaer.menuw.domain.repository.MenuRepository
+import javax.inject.Inject
+
+class MenuRepositoryImpl @Inject constructor(private val menuDataSource: MenuDataSource): MenuRepository {
+
+    override suspend fun postMenuRecipe(menuName: String): Result<RecipeList> = runCatching {
+        menuDataSource.postMenuRecipe(menuName).toRecipeList()
+    }
+}
