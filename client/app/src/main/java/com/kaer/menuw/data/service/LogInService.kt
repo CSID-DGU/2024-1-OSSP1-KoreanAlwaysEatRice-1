d0633d9d81c31e59837748bf064ba5f4da067226
emjayMJkim <minjeong030420@gmail.com>
2024-05-30 02:01:06 +0900

    #38 [add] service & dto 생성

---
+++ client/app/src/main/java/com/kaer/menuw/data/service/LogInService.kt
@@ -0,0 +1,14 @@
+package com.kaer.menuw.data.service
+
+import com.kaer.menuw.data.model.remote.response.ResponsePostRecipeDto
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface LogInService {
+
+    @POST("/login")
+    suspend fun postLogIn(
+        @Header("Content-Type") contentType: String,
+        @Header("Authorization") authorization: String
+    ): ResponsePostRecipeDto
+}
