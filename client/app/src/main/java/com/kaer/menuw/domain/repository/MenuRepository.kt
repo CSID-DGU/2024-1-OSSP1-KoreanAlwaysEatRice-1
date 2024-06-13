7aa650877b7501db68f22dd46b0a53fcb432322a
emjayMJkim <minjeong030420@gmail.com>
2024-06-13 21:13:42 +0900

    #66 [add] dataSource & repository 생성

--- client/app/src/main/java/com/kaer/menuw/domain/repository/MenuRepository.kt
+++ client/app/src/main/java/com/kaer/menuw/domain/repository/MenuRepository.kt
@@ -2,10 +2,17 @@ package com.kaer.menuw.domain.repository
 
 import com.kaer.menuw.domain.entity.LikeMenu
 import com.kaer.menuw.domain.entity.RecipeList
+import com.kaer.menuw.domain.entity.RecommendMenu
 
 interface MenuRepository {
 
     suspend fun postMenuRecipe(menuName: String): Result<RecipeList>
 
     suspend fun getLikeMenuList(): Result<List<LikeMenu>>
+
+    suspend fun postRecommendMenuList(
+        recipe: String,
+        menuType: String,
+        ingredientList: ArrayList<Int>
+    ): Result<List<RecommendMenu>>
 }
