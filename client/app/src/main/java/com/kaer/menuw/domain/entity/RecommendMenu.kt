464d4023f195bfcbd54ef2f714076f38e056dcfa
emjayMJkim <minjeong030420@gmail.com>
2024-06-13 21:26:10 +0900

    #66 [mod] recommendMenu data class 구조 변경

--- client/app/src/main/java/com/kaer/menuw/domain/entity/RecommendMenu.kt
+++ client/app/src/main/java/com/kaer/menuw/domain/entity/RecommendMenu.kt
@@ -2,7 +2,11 @@ package com.kaer.menuw.domain.entity
 
 data class RecommendMenu (
     val menuId: Int,
-    val menuImgUrl: String,
     val menuName: String,
-    val ingredientList: String
+    val menuImgUrl: String,
+    val ingredients: String,
+    val cal: Int,
+    val na: Int,
+    val similarity: Double,
+    val recommend: Boolean
 )
